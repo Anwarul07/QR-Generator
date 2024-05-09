7803168999c3e6b0cd9e60c030dd4d04d24a7027
Anwarul07 <mdanwarul064@gmail.com>
2024-05-09 23:59:23 +0530

    Update script.js

--- scripyt.js
+++ scripyt.js
@@ -1,1 +1,15 @@
 console.log(" welcome to qr generator");
+
+let imgbox= document.getElementById("imgBox");
+let qrtext= document.getElementById("qrtext");
+let qrimg= document.getElementById("qr")
+
+function generate(){
+
+    if(qrtext.value.length>0){
+        qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrtext.value;
+        imgbox.classList.add("show");
+    }
+ 
+
+}
