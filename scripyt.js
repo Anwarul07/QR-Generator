6845be2d54bd276a45adda4a989104a26f4dcdaf
Anwarul07 <mdanwarul064@gmail.com>
2024-05-10 00:07:43 +0530

    Update script.js

--- scripyt.js
+++ scripyt.js
@@ -9,6 +9,11 @@ function generate(){
     if(qrtext.value.length>0){
         qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrtext.value;
         imgbox.classList.add("show");
+    } else{
+        qrtext.classList.add("error");
+        setTimeout(() => {
+            qrtext.classList.remove("error");  
+        }, 1000);
     }
  
 
